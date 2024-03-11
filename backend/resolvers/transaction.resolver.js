63c3e8177bccda3090bcc23c8f0468b59285616a
Enayet <enayetflweb@gamil.com>
2024-03-11 09:03:52 +0600

    query and mutation done inside transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,73 @@
+import Transaction from "../models/transaction.model"
+
 const transactionResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    transactions: async (_,_, context ) => {
+      try {
+        if(!context.getUser()) {
+          throw new Error("Unauthorized")
+        }
+        const userId = await context.getUser()._id
+        const transactions = await Transaction.find({ userId})
+        return transactions
+      } catch (error) {
+        console.error("Error getting transactions", error)
+        throw new Error("Error getting Transactions")
+      }
+    },
+    transaction: async(_, {transactionId}) => {
+      try {
+       
+        const transaction = await Transaction.findById({transactionId})
+        return transaction
+      } catch (error) {
+        console.error("Error getting transaction", error)
+        throw new Error("Error getting Transactions")
+      }
+    },
+
+  },
+  Mutation: {
+    createTransaction: async(_, {input},context) => {
+      try {
+        
+
+      const newTransaction = new Transaction({...input,
+      userId: context.getUser()._id
+      })
+
+       await newTransaction.save()
+
+      return newTransaction
+      } catch (error) {
+        console.error("Error creating transaction", error)
+        throw new Error("Error creating transaction")
+      }
+
+    },
+    updateTransaction: async(_, {input}) => {
+        try {
+          
+          const updateTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {new: true})
+
+          return updateTransaction
+        } catch (error) {
+          console.error("Error updating transaction", error)
+          throw new Error("Error updating transaction")
+        }
+    },
+    deleteTransaction: async(_, {transactionId},) => {
+      try {
+          
+        const deleteTransaction = await Transaction.findByIdAndDelete(transactionId)
+
+        return deleteTransaction
+      } catch (error) {
+        console.error("Error deleting transaction", error)
+        throw new Error("Error deleting transaction")
+      }
+    },
+  },
 }
 
 export default transactionResolver
